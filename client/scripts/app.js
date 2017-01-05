a2d34c1dc62ec44de0a6d290bf18093245d3fde6
msermersheim <m.sermersheim@gmail.com>
2017-01-04 17:42:36 -0800

    tests through Movie Collection passing

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,7 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    this.set('like', !this.get('like'));  
   }
 
 });
@@ -15,13 +15,16 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    this.on('change:like', function() {
+      this.sortByField('like');
+    });
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    this.comparator = field;
+    this.sort(field);
   }
 
 });
