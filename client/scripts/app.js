5a06287df74312ad4a4a98671afd45afac5ed2f8
msermersheim <m.sermersheim@gmail.com>
2017-01-04 20:16:30 -0800

    Pass all tests for siskel

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -61,7 +61,9 @@ var MovieView = Backbone.View.extend({
                         </div>'),
 
   initialize: function() {
-    // your code here
+    this.model.on('change:like', function() {
+      this.render();
+    }, this);
   },
 
   events: {
@@ -69,7 +71,7 @@ var MovieView = Backbone.View.extend({
   },
 
   handleClick: function() {
-    // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -82,7 +84,9 @@ var MovieView = Backbone.View.extend({
 var MoviesView = Backbone.View.extend({
 
   initialize: function() {
-    // your code here
+    this.collection.on('sort', function() {
+      this.render();
+    }, this);
   },
 
   render: function() {
